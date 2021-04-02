ef956ac3edac852ff9b28686bdf5a37b2adbbdfa
AliLorentz <ajosue998@gmail.com>
2021-04-02 00:16:27 -0430

    Helper para ayudar a traer los datos

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const routes = require('./routes/index.js');
 const path = require('path')
 const bodyParser = require('body-parser');
+
+//helpers
+const helpers = require('./helper.js');
+
 //crear la conexion a la bd
 const db=require('./config/db.js');
 //importar el modelo
@@ -18,8 +22,15 @@ app.use(express.static('public'));
 
 //Habilitar Pug
 app.set('view engine','pug')
+
 //añadir la carpeta a las vistas
 app.set('views',path.join(__dirname,'./views'));
+
+//pasar var dump a la aplicacion
+app.use((req,res,next)=>{
+	res.locals.vardump = helpers.vardump;
+	next();
+})
 
 //Habilitar bodyParser para leer los datos enviados al formulario
 app.use(bodyParser.urlencoded({extend:true}))
